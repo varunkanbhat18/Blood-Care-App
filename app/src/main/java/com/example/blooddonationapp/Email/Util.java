b131487681ef3720eb3a50483eb3f9b0276dcebb
varunkanbhat18 <varun.kanbhat@somaiya.edu>
2021-12-08 17:42:32 +0530

    Project is added

---
+++ app/src/main/java/com/example/blooddonationapp/Email/Util.java
@@ -0,0 +1,6 @@
+package com.example.blooddonationapp.Email;
+
+public class Util {
+    public  static  final String EMAIL = "blooddonationappas@gmail.com";
+    public static final String PASSWORD = "bdaas2021";
+}
